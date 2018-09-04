462e180b7bbbfd9d0d9517bdb555ef9ee16201cd
Zero <matt.zerostudios@gmail.com>
2018-09-04 10:29:19 -0500

    updated utilities

--- utilities/utillities.py
+++ utilities/utillities.py
@@ -1,4 +1,8 @@
 import os
+import re
+import time
+
+current_module = ''
 
 
 def fetch_keys():
@@ -6,18 +10,35 @@ def fetch_keys():
 
 
 def output(message):
-    print('> %s' % message)
-    idle()
+    global current_module
+    if current_module == '':
+        print('> %s' % message)
+        time.sleep(1)
+        idle()
+    else:
+        print('>/%s/ %s' % (current_module, message))
+        idle()
+
 
 def idle():
-    __response = input('>')
-    options(__response)
+    global  current_module
+    if current_module == '':
+        __response = input('>/%s/ ' % current_module)
+        options(__response)
+    else:
+        __response = input('> ')
+        options(__response)
 
 
 def options(response):
     if response == "Help" or response == "help":
         output("This is the help screen")
-    elif response == "exit()":
+    elif "use=" in response:
+        global current_module
+        rand, __module = re.split('=', response)
+        current_module = __module
+
+    elif response == "exit":
         os._exit(0)
     else:
         output("Im sorry I didn't quite catch that")
