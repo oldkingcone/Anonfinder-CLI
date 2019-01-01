d7f84cf03cc869320b912f4bdb0c0d95f6d87350
Zero <matt.zerostudios@gmail.com>
2018-12-31 21:22:37 -0600

    new structure

--- anonfinder.py
+++ anonfinder.py
@@ -1,49 +1,55 @@
 from utilities import utilities
-from utilities.twitter import Twitter
+
+workspace = ''
+target = ''
 
 
-class Manager(utilities.Options):
-
-    obj_names = ["twitter", "back", "fullcontact", "google", "exit"]
-
-    def __init__(self):
-        self.queue = [utilities.AnonFinder(), Twitter()]
-        self.current_obj = self.fetch(0)
-        self.name = self.current_obj.name
-        self.menu()
-
-    def stow(self, obj):
-        self.Obj_list.append(obj)
-
-    def fetch(self, *index):
-        return self.Obj_list.pop(*index)
-
-    def menu(self):
-
-        while self.alive:
-            response = self.get_input()
-
-            if response in self.obj_names:
-                if response == "twitter":
-                    self.stow(self.current_obj)
-                    self.fetch()
-                    self.name = self.current_obj.name
-                else:
-                    pass
-
-                if response == "exit":
-                    self.exit()
-                else:
-                    pass
-
-                if "back" in response:
-                    self.stow(self.current_obj)
-                    self.fetch(0)
-                else:
-                    pass
-
-            else:
-                self.current_obj.menu(response)
+def greeting():
+    output('Welcome to AnonFinder, type help to get started', 'AnonFinder')
 
 
-manager = Manager()
+def output(msg, *prompt):
+    if prompt:
+        print("[%s]> %s" % (prompt[0], msg))
+    else:
+        print("> %s" % msg)
+
+
+def make_profile(option):
+    pass
+
+
+def delete_profile(option):
+    pass
+
+
+def run():
+    pass
+
+
+def use(option):
+    pass
+
+
+def set_value(param, option):
+    pass
+
+
+def exit_program():
+    pass
+
+
+def exclude(*option):
+    pass
+
+
+def help_menu():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
